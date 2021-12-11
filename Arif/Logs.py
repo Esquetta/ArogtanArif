3f1eec839ce843d1b7f07000cb17a46d6fd53a04
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2021-12-11 21:03:56 +0300

    Update

--- Arif/Logs.py
+++ Arif/Logs.py
@@ -1,5 +1,5 @@
 import datetime
-
+import discord
 from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import UserInputError
@@ -11,22 +11,24 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.log_channel_id = 0
+        self.log_channel_id=None
         self.log_channel = self.bot.get_channel(self.log_channel_id)
 
-    @commands.command(name="setLogChannel", help="Define log channel with channel id")
-    async def set_log_channel(self, ctx, channel_id: int):
-        try:
-            if len(str(channel_id))==18:
-                self.log_channel = self.bot.get_channel(channel_id)
-                await ctx.send("👍")
-            else:
-                await  ctx.send("Invlaid text channel id.Id length must be 18 numbers")
-        except UserInputError:
-            await  ctx.send("You must enter numeric channel id")
+    @commands.command(name="setupLogChannel", help="Define log channel with channel id",
+                      aliales=["setlogChannel", "LogChannelSetup"])
+    async def setup_log_channel(self, ctx):
+        for channel in ctx.guild.text_channels:
+            if channel.name == "log":
+                await  ctx.send("Channel exist.")
+        overwrites = {ctx.guild.default_role: discord.PermissionOverwrite(read_messages=True),
+                      ctx.guild.me: discord.PermissionOverwrite(read_messages=True)}
+        log_channel = await  ctx.guild.create_text_channel('Logs', overwrites=overwrites)
+        self.log_channel_id=log_channel.id
+        await  ctx.send("Setup completed.")
 
     @commands.Cog.listener()
     async def on_user_update(self, before, after):
+        self.log_channel=self.bot.get_channel(self.log_channel_id)
         if before.avatar_url != after.avatar_url:
             embed = Embed(title="Member update", description="Avatar Change  =>Is old one", colour=after.colour,
                           timestamp=datetime.datetime.utcnow())
@@ -40,6 +42,7 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
+        self.log_channel = self.bot.get_channel(self.log_channel_id)
         if before.display_name != after.display_name:
             embed = Embed(title="Member update", description="Nickname Change", colour=after.colour,
                           timestamp=datetime.datetime.utcnow())
@@ -60,7 +63,6 @@ class Logs(commands.Cog):
             for name, value, inline in fields:
                 embed.add_field(name=name, value=value, inline=inline)
             await self.log_channel.send(embed=embed)
-
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
