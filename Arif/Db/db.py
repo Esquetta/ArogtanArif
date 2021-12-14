497c54f99b3c1c7c06369dc22916b15b0a34f813
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2021-12-14 19:07:49 +0300

    Update db.py

--- Arif/Db/db.py
+++ Arif/Db/db.py
@@ -1,7 +1,6 @@
 import  pypyodbc
 
 DB_PATH="DRIVER={SQL Server};SERVER=DESKTOP-EMPIGDT;DATABASE=CarGallery;Trusted_Connection=yes;"
-    #"Data Source=DESKTOP-EMPIGDT;Initial Catalog=CarGallery;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
 con=pypyodbc.connect(DB_PATH)
 cursor=con.cursor()
 cursor.execute("Select * From Cars")
