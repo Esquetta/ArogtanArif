4e88d8a65f0dfec2a797721f9f588bc35094b9ae
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2021-12-14 19:12:06 +0300

    Update

--- Arif/Db/db.py
+++ Arif/Db/db.py
@@ -1,12 +1,16 @@
-import  pypyodbc
+import pypyodbc
 
-DB_PATH="DRIVER={SQL Server};SERVER=DESKTOP-EMPIGDT;DATABASE=CarGallery;Trusted_Connection=yes;"
-con=pypyodbc.connect(DB_PATH)
-cursor=con.cursor()
-cursor.execute("Select * From Cars")
-values=cursor.fetchall()
+DB_PATH = "DRIVER={SQL Server};SERVER=DESKTOP-EMPIGDT;DATABASE=CarGallery;Trusted_Connection=yes;"
+con = pypyodbc.connect(DB_PATH)
+cursor = con.cursor()
 
-for item in values:
-    print(item)
 
-con.close()
+def Get_all():
+    cursor.execute("Select * from Cars")
+    values = cursor.fetchall()
+    for item in values:
+        print(item)
+    con.close()
+
+
+
