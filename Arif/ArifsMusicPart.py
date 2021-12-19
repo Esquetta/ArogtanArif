2d2dfc80104666241c40e2180380611e7faaa4d3
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2021-12-19 16:40:23 +0300

    Game

--- Arif/ArifsMusicPart.py
+++ Arif/ArifsMusicPart.py
@@ -5,6 +5,7 @@ import Information
 import Logs
 from Db.Entities.Servers import Servers
 from Db.db import Set_Server
+import  Game
 from flaskserver import live
 import os
 from dotenv import load_dotenv
@@ -13,7 +14,7 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 prefix = "Arif."
 OWNER_IDS = [276761363022479361]
-cogs = [music, Information, Logs]
+cogs = [music, Information, Logs,Game]
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix=prefix, intents=intents, owner_ids=OWNER_IDS)
 client.remove_command("help")
