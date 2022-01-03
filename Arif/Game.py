c2880c2919437c6e199332e1defaa404be74cc73
Furkan Taşçı <67598507+Esquetta@users.noreply.github.com>
2022-01-03 19:59:34 +0300

    Sending gifs with specified word or random.

--- Arif/Game.py
+++ Arif/Game.py
@@ -1,8 +1,15 @@
+import json
+import os
+import urllib.request
+from typing import Optional
+import requests
 from discord import Embed
 from discord.ext import commands
 import random
 
 choices = ["Rock", "Paper", "Scissors"]
+lmt = 15
+tenor_key = os.getenv("TENOR_KEY")
 
 
 class Game(commands.Cog):
@@ -38,15 +45,34 @@ class Game(commands.Cog):
         else:
             await ctx.send("You must chose Rock-Paper-Scissors.")
 
-    @commands.command(name="CoinFlip",aliases=["coinflip"])
+    @commands.command(name="CoinFlip", aliases=["coinflip"])
     async def CoinFlip(self, ctx):
         if random.randint(0, 1) == 0:
-            embed=Embed(title="CoinFlip",description=f"{ctx.author.mention} Flipped coin, we got **Heads**!")
+            embed = Embed(title="CoinFlip", description=f"{ctx.author.mention} Flipped coin, we got **Heads**!")
             await ctx.send(embed=embed)
         else:
             embed = Embed(title="CoinFlip", description=f"{ctx.author.mention} Flipped coin, we got **Tails**!")
             await ctx.send(embed=embed)
-    '''Gif Alanı gelicek tenor api ile search veya random git yollanmalı'''
+
+    @commands.command(name="Gif", aliases=["gif"])
+    async def sendGif(self, ctx,*, search_term: Optional[str]):
+        search_term= search_term or ""
+        if search_term is not None:
+            r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, tenor_key, lmt))
+            if r.status_code == 200:
+                top_8gifts_first = json.loads(r.content)
+                await ctx.send(f"{top_8gifts_first['results'][random.randint(0,15)]['url']}")
+        else:
+            word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+            response = urllib.request.urlopen(word_site)
+            txt = response.read()
+            WORDS = txt.splitlines()
+            search_term=random.choice(WORDS)
+            r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, tenor_key, lmt))
+            if r.status_code == 200:
+                top_8gifts_first = json.loads(r.content)
+                await ctx.send(f"{top_8gifts_first['results'][random.randint(0, 15)]['url']}")
+
 
 def setup(client):
     client.add_cog(Game(client))
