41c5299ffb2e565de49523fca084ddbc3aaa457f
esquetta <esquetta@gmail.com>
2022-09-25 17:36:32 +0300

    Fixing issues

--- Arif/Information.py
+++ Arif/Information.py
@@ -110,7 +110,7 @@ class Information(commands.Cog):
                   ("Info:information_source:", "userinfo,Svinfo,avatar,roleinfo,ping", True),
                   ("Log:pencil: ", "setupLogChannel or LogChannelSetup  creates log text channel", True),
                   ("Game:game_die: ", "Rock-Paper-Scissors,CoinFlip,NumberGuess,Bigtext,CoinFlip", "True"),
-                  ("Fun:tada:", "Gif", "True"),
+                  ("Fun:tada:", "Gif","Photo", "True"),
                   ("More", "Arif.help 'command name' extented help with specified command.", "True")
 
                   ]
