a3f30dee82ba6b0cd81be392d00632e195247f6e
Esquetta <esquetta@gmail.com>
2023-02-16 21:02:28 +0300

    Chat command max tokes reduces 1000 to 300.AI answers more stable.

--- Arif/Chat.py
+++ Arif/Chat.py
@@ -10,14 +10,15 @@ class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="chat",with_app_command=True, help="Ask any question to Arif",pass_context=True)
+    @commands.hybrid_command(name="chat", with_app_command=True, help="Ask any question to Arif,it could may take a few minitues to answer.", pass_context=True)
     async def Chat(self, ctx, *, question: str):
+
         async  with ctx.typing():
             response = openai.Completion.create(model="text-davinci-003", prompt=question, temperature=0,
-                                            max_tokens=1000)
-            await  asyncio.sleep(120)
+                                                max_tokens=300)
+            await  asyncio.sleep(60)
 
-        await  ctx.send(response.choices[0].text,ephemeral= True)
+        await  ctx.send(response.choices[0].text, ephemeral=True)
 
 
 async def setup(bot):
