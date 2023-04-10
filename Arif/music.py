f83cc6612b667732e44f7a8e5a389acde42bbc2b
Esquetta <esquetta@gmail.com>
2023-04-10 20:30:57 +0300

    Fixing issues,youtube dl is not working we are waiting for new update,for now play command is not working

--- Arif/music.py
+++ Arif/music.py
@@ -18,7 +18,7 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'outtmpl': 'Musics/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
